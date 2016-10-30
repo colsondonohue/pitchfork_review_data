60b4f7f76e919764fa9d06c741b6e88d9d492628
Colson Donohue <colsondonohue@gmail.com>
2016-10-30 17:22:25 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-This is a scraper that runs on [Morph](https://morph.io). To get started [see the documentation](https://morph.io/documentation)
+This is a scraper that runs on [Morph](https://morph.io).
+It pulls in review data from Pitchfork including artist, album, review date,
+reviewer, scores, etc. This data is made accessible as a .csv or .sqlite file, or through Morph.io's API.
--- scraper.py
+++ scraper.py
@@ -1,24 +1,61 @@
-# This is a template for a Python scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
 
-# import scraperwiki
-# import lxml.html
-#
-# # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
-#
-# # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
-#
-# # Write out to the sqlite database using scraperwiki library
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
-#
-# # An arbitrary query against the database
-# scraperwiki.sql.select("* from data where 'name'='peter'")
+import scraperwiki
+import lxml.html
 
-# You don't have to do things with the ScraperWiki and lxml libraries.
-# You can use whatever libraries you want: https://morph.io/documentation/python
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+def parse(url, modifier, page=''):
+    html = scraperwiki.scrape(url + modifier + page, user_agent='Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7')
+    page = lxml.html.fromstring(html)
+    return page
+
+def scrape_review(url, href):
+    page = parse(url, href)
+    url = url + href
+    album = page.cssselect('h1.review-title')[0].text_content()
+
+    artists_el = page.cssselect('ul.artist-list.artist-links li a')
+    artists = ''
+    for artist in artists_el:
+        artists += artist.text_content() + ' '
+
+    score = page.cssselect('span.score')[0].text_content()
+    date = page.cssselect('span.pub-date')[0].get('title')
+    reviewer = page.cssselect('a.display-name')[0].text_content()
+
+    genres_el = page.cssselect('ul.genre-list li a')
+    genres = ''
+    for genre in genres_el:
+        genres += genre.text_content() + ' '
+
+    unique_keys = ['url']
+    data = {
+            'url': url,
+            'album': album,
+            'artists': artists,
+            'score': score,
+            'date': date,
+            'reviewer': reviewer,
+            'genres': genres
+            }
+
+    scraperwiki.sqlite.save(unique_keys, data)
+
+
+def scrape_page(url, page):
+    page = parse(url, '/reviews/albums/?page=', page)
+    if page.cssselect('div.four-oh-four'):
+        return False
+    links = page.cssselect('a.album-link')
+    for link in links:
+        href = link.get('href')
+        scrape_review(url, href)
+
+
+
+base_url = 'http://pitchfork.com'
+current_page = 1
+
+
+while True:
+    if not scrape_page(base_url, str(current_page)):
+        break
+    current_page += 1
